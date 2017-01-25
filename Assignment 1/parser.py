a28632c07d646ffac252d744eea417957f7699ff
sreeraj201 <sreeraj201@gmail.com>
2017-01-24 20:44:34 -0600

    documented

--- Assignment 1/parser.py
+++ Assignment 1/parser.py
@@ -5,7 +5,7 @@ def parse_input(filename = 'input-data.txt'):
     """
         Parses input file
         Input: filename
-        Returns transaction database 
+        Returns transaction database (list of lists) 
     """
     with open(filename) as f:
         contents = f.readlines()
@@ -27,20 +27,22 @@ def parse_input(filename = 'input-data.txt'):
 
 def switch_parameters(x):
     """
-        Select parameter key 
-        Return key and value
+        Returns key and value
     """
     if re.search('MIS.+',x):
         tmp = re.search('MIS\((.+)\) = (.+)',x)
         key,val = int(tmp.group(1)),float(tmp.group(2))
+
     elif re.search('SDC.+',x):
         key = 'SDC'
         val = re.search('SDC = (.+)',x).group(1)
         val = float(val)
+
     elif re.search('cannot_be_together.+',x):
         key = 'cannot_be_together'
         tmp = re.search('cannot_be_together: (.+)',x).group(1)
         tmp = re.findall('{(\d+, \d+)}',tmp)
+        # split by comma
         val = [i.split(',') for i in tmp]
         # convert to int
         val = [[int(j) for j in i] for i in val]
@@ -49,17 +51,22 @@ def switch_parameters(x):
         key = 'must_have'
         tmp = re.search('must-have: (.+)',x).group(1)
         val = re.findall('\d+',tmp)
+        # convert to int
         val = [int(i) for i in val]
 
     return key,val
-
 
 
 def parse_parameter(filename = 'parameter-file.txt'):
     """
         Parses parameter file
         Input : Filename
-        Output : 
+        Returns dictionary {
+            "MIS" : dict
+            'SDC' : int
+            'cannot_be_together': list of lists
+            'must_have': list
+        }
     """
     with open(filename) as f:
         contents = f.readlines()
@@ -68,9 +75,10 @@ def parse_parameter(filename = 'parameter-file.txt'):
     param_dict['MIS'] = {}
 
     for content in contents:
+        # get key and value of parameters
         key,value = switch_parameters(content) 
+        # if int then add to MIS dict
         if isinstance(key,int):
-            key = key
             param_dict['MIS'][key] = value
         else:
             param_dict[key] = value
