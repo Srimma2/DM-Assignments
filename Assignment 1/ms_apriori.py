802ff68a7ca67194e82cc8dc79e002ce6e8ddb23
sreeraj201 <sreeraj201@gmail.com>
2017-01-30 22:01:21 -0600

    added modifications

--- Assignment 1/ms_apriori.py
+++ Assignment 1/ms_apriori.py
@@ -1,7 +1,9 @@
 import sys,os,re,operator,itertools
 from collections import defaultdict
 
-#TODO - normalize the counts, 2nd level candidate gen should take arguement L
+from parser import *
+
+#TODO - normalize the counts, 2nd level candidate gen should take argument L
 # make sure that everything is passed into functions is sorted according to MIS values
 
 def lvl2_candidategen(itemset_dict,phi,param_dict):
@@ -9,9 +11,9 @@ def lvl2_candidategen(itemset_dict,phi,param_dict):
     items = sorted(itemset_dict.items(), key=operator.itemgetter(1)) #sorted list of tuples of [(key,value)....]
     setLen = len(items)
     for i in range(0,setLen): #each item i in L
-        if items[i][1] >= param_dict['MIS'][items[i]]:
+        if items[i][1] >= param_dict['MIS'][items[i][0]]:
             for h in range(i+1,setLen): # each item after i in L
-                if items[h][1] >= param_dict['MIS'][items[i]] and abs(items[h][1]-items[i][1]) <= phi:
+                if items[h][1] >= param_dict['MIS'][items[i][0]] and abs(items[h][1]-items[i][1]) <= phi:
                     candidate = [items[i][0],items[h][0]] # {i,h}
                     c2.append(candidate)
     
@@ -19,7 +21,7 @@ def lvl2_candidategen(itemset_dict,phi,param_dict):
     
 def MS_candidategen(f,phi,param_dict):
     ck = []
-    setLen = len(f) #lenght of f
+    setLen = len(f) #length of f
     eLen = len(f[0])-1 #length of the element
     for i in range(0,setLen): #each item i in L
             for h in range(i+1,setLen): #each item h after i
@@ -38,49 +40,48 @@ def MS_candidategen(f,phi,param_dict):
                                 ck.remove(c) 
     return ck
 
-def MS_Apriori(transaction_db,param_dict,phi):
+def MS_Apriori(transaction_db,param_dict):
     """
         MS Apriori Algorithm
         Input : 
             transaction_db : transaction database
             param_dict : paramter dictionary 
                         {MIS,SDC,cannot_be_together,must_have}
-            phi : difference constraint
         Returns frequent_itemsets : dict
     """
     # contains all itemset with their count initialized to 0; key = itemset(tuple), value = list
-    itemset_dict = defaultdict(lambda : 0)
+    itemset_dict = defaultdict(lambda : 0.0)
     # lists frequent itemsets (list) at each level; key = F_<level>, value = list
     frequent_itemsets = dict()
     # lists candidates (list) at each level; key = C_<level>, value = list
     candidate_itemsets = dict()
 
+    # support difference constraint
+    phi = param_dict['SDC']
+
     # get items sorted by MIS
     M = sorted(param_dict['MIS'], key = param_dict['MIS'].get)
 
     # size of transaction
-    n = len(transaction_db)
+    n = float(len(transaction_db))
 
-    # initial pass
-    # count occurences  
-    for key in M.keys():
-        count = 0
-        for t in transaction_db:
-            if key in t:
-                count += 1
-                continue
-        # convert key to tuple
-        itemset_dict[tuple(key)] = count
+    for t in transaction_db:
+        for item in M:
+            if item in t:
+                itemset_dict[item] += 1/n
 
     # frequent itemset
-    frequent_itemsets['F_1'] = [list(key) for key,val in itemset_dict.iteritems() if val > param_dict['MIS'][key[0]]]
+    frequent_itemsets['F_1'] = [[key] for key,val in itemset_dict.iteritems() if val > param_dict['MIS'][key]]
 
     k = 2
 
     while(frequent_itemsets['F_'+str(k-1)]):
         if k == 2:
+            # remove items with support less than min of MIS
+            L = {key:val for key,val in itemset_dict.iteritems() if val > param_dict['MIS'][M[0]]}
             # list of candidates(list)
-            candidate_itemsets['C_'+str(k)] = lvl2_candidategen(itemset_dict,phi,param_dict)
+            candidate_itemsets['C_'+str(k)] = lvl2_candidategen(L,phi,param_dict)
+            # print candidate_itemsets['C_' + str(k)]
         else:
             candidate_itemsets['C_'+str(k)] = MS_candidategen(frequent_itemsets['F_'+str(k-1)],phi,param_dict)
 
@@ -88,13 +89,21 @@ def MS_Apriori(transaction_db,param_dict,phi):
             for c in candidate_itemsets['C_'+str(k)]:
                 # if c is contained in t
                 if set(c) < set(t):
-                    itemset_dict[tuple(c)] += 1
+                    # normalize
+                    itemset_dict[tuple(c)] += 1/n
                 # if c without first element is contained in t
-                if set(c[1:]) < set(t):
-                    itemset_dict[tuple(c[1:])] += 1
+                # for rule generation
+                # if set(c[1:]) < set(t):
+                #     itemset_dict[tuple(c[1:])] += 1/n
 
-        frequent_itemsets['F_'+str(k)] = [c for c in candidate_itemsets['C_'+str(k)] if itemset_dict[tuple(c)]/n >= param_dict['MIS'][c[1:]]]
+        # sort based on MIS
+        frequent_itemsets['F_'+str(k)] = [sorted(c,key = param_dict['MIS'].get) for c in candidate_itemsets['C_'+str(k)] if itemset_dict[tuple(c)] >= param_dict['MIS'][c[1]]]
         k += 1
 
     return frequent_itemsets
 
+
+if __name__ == "__main__":
+    transaction_db = parse_input('input-data.txt')
+    param_dict = parse_parameter('parameter-file.txt')
+    print MS_Apriori(transaction_db,param_dict)
--- Assignment 1/tests/test_apriori.py
+++ Assignment 1/tests/test_apriori.py
@@ -1,7 +1,12 @@
 import unittest
 
 from ms_apriori import *
+from parser import *
 
 class Apriori_Test(unittest.TestCase):
     def setUp(self):
-        pass
+        self.transaction_db = parse_input('tests/input-data.txt')
+        self.param_dict = parse_parameter('tests/parameter-file.txt')
+
+    def test_apriori(self):
+        frequent_itemsets = MS_Apriori(self.transaction_db, self.param_dict)
