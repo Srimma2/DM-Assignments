2dff4ed9d26257b77851e953486a61a62a974508
jdeuts4 <jdeuts4@uic.edu>
2017-04-16 04:21:07 -0500

    Update BasicLSTM.py

--- Assignment 2/BasicLSTM.py
+++ Assignment 2/BasicLSTM.py
@@ -24,9 +24,10 @@ from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.snowball import SnowballStemmer
 
-from sklearn.metrics import classification_report,accuracy_score
-from sklearn.cross_validation import cross_val_score,cross_val_predict
+from sklearn.metrics import classification_report,accuracy_score,f1_score
+from sklearn.cross_validation import cross_val_score,cross_val_predict,KFold
 
+np.random.seed(1)
 
 MAX_SEQUENCE_LENGTH = 30 #max number of sentences in a message
 MAX_NB_WORDS = 20000 #cap vocabulary
@@ -36,6 +37,21 @@ TWITTER_FILE = '/Users/nookiebiz4/583_proj2/training-Obama-Romney-tweets.xlsx'
 JAR_FILE = '/Users/nookiebiz4/Downloads/stanford-postagger-2016-10-31/stanford-postagger.jar'
 MODEL_FILE = '/Users/nookiebiz4/Downloads/stanford-postagger-2016-10-31/models/english-left3words-distsim.tagger'
 
+
+def get_Ytrue_Ypred(model,x,y):
+    #Y matrix is [1,0,0] for class 0, [0,1,0] for class 1, [0,0,1] for class -1
+    convert_to_label ={0:0,1:1,2:-1}
+    model_predictions = model.predict(x)
+    y_pred = np.zeros(len(y))
+    y_true = np.zeros(len(y))
+    #errors = 0.0
+    for i in range(len(y)):
+        y_pred[i] = convert_to_label[np.argmax(model_predictions[i])]
+        y_true[i] = convert_to_label[np.argmax(y[i])]
+        #if y_true[i] != y_pred[i]:
+            #errors+=1.0
+    return y_true,y_pred
+    
 
 # read the data
 obama_data = pd.read_excel(TWITTER_FILE,names = ['date','time','text','sentiment'],parse_cols = 4,sheetname = 'Obama')
@@ -235,10 +251,9 @@ def get_X_y(data):
 
 
 
-VALIDATION_SPLIT = .60
 
 texts = obama_data['text']
-labels = obama_data['sentiment']
+labels = np.array(obama_data['sentiment'])
 
 tokenizer = keras.preprocessing.text.Tokenizer(nb_words=MAX_NB_WORDS)
 tokenizer.fit_on_texts(texts)
@@ -249,24 +264,12 @@ print('Found %s unique tokens.' % len(word_index))
 
 #pad the data 
 data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+print labels[0:4]
+labels = keras.utils.np_utils.to_categorical(labels,nb_classes=3)
+print labels[0:4]
 
-labels = keras.utils.np_utils.to_categorical(np.asarray(labels))
 print('Shape of data tensor:', data.shape)
 print('Shape of label tensor:', labels.shape)
-
-# split the data into a training set and a validation set
-indices = np.arange(data.shape[0])
-np.random.shuffle(indices)
-data = data[indices]
-labels = labels[indices]
-nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
-
-
-
-x_train = data[:-nb_validation_samples]
-y_train = labels[:-nb_validation_samples]
-x_val = data[-nb_validation_samples:]
-y_val = labels[-nb_validation_samples:]
 
 
 
@@ -293,35 +296,42 @@ for word, i in word_index.items():
         # words not found in embedding index will be all-zeros.
         embedding_matrix[i] = embedding_vector
 
+
+#create the embedding layer
 embedding_layer = Embedding(num_words,
                             EMBEDDING_DIM,
                             weights=[embedding_matrix],
                             input_length=MAX_SEQUENCE_LENGTH,
                             trainable=False)
 
-
 # create the model
-model = Sequential()
-model.add(embedding_layer)
-model.add(LSTM(100))
-model.add(Dense(len(y_train[0]), activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam')
-model.fit(x_train, y_train, nb_epoch=30, batch_size=64)
+np.random.seed(1)
 
-model_predictions = model.predict(x_val)
-y_pred = np.zeros(len(y_val))
-y_true = np.zeros(len(y_val))
-errors = 0.0
-for i in range(len(y_val)):
-    y_pred[i] = np.argmax(model_predictions[i])
-    y_true[i] = np.argmax(y_val[i])
-    if y_true[i] != y_pred[i]:
-        errors+=1.0
-          
-       
-print classification_report(y_true,y_pred)
-print 1.0-errors/len(y_val)
 
+
+#k fold cross validaiton
+avg_acc = []
+avg_f1 = []
+kf = KFold(n=len(data),n_folds=10)
+for train,test in kf:
+    np.random.seed(1)
+    x_train, x_val, y_train, y_val = data[train], data[test], labels[train], labels[test]
+    model = Sequential()
+    model.add(embedding_layer)
+    model.add(LSTM(10))
+    model.add(Dense(len(labels[0]), activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+    model.fit(x_train, y_train, nb_epoch=30, batch_size=64,verbose=False)
+    y_true,y_pred = get_Ytrue_Ypred(model,x_val,y_val)
+    avg_acc.append(accuracy_score(y_true,y_pred))
+    avg_f1.append(f1_score(y_true,y_pred))      
+    print classification_report(y_true,y_pred)
+
+
+#print classification_report(y_true,y_pred)
+print 'Average f1-score = ', np.mean(np.array(avg_f1))
+print 'Overall Accuracy = ',100.0*np.mean(np.array(avg_acc)),'%'
 
 
 
